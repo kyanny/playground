53a3488a0083f2ed1ba9065b757457cd0b48f300
Kensuke Nagae <kyanny@gmail.com>
2015-09-18 01:52:07 +0900

    Dragonfly Rack

---
+++ ruby-dragonfly/rack/gen_url.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+# Usage: ./gen_url.rb
+require 'dragonfly'
+Dragonfly.app.configure do
+  verify_urls false
+end
+uid = Dragonfly.app.store(File.open('lena_std.png'))
+url_path = Dragonfly.app.fetch(uid).url
+puts "http://localhost:9292" + url_path
