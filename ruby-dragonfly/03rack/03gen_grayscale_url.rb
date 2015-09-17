efcd9bcaf90054b3f638626a155fd0e7b6c19a18
Kensuke Nagae <kyanny@gmail.com>
2015-09-18 02:11:15 +0900

    Fixes

--- ruby-dragonfly/03rack/03gen_grayscale_url.rb
+++ ruby-dragonfly/03rack/03gen_grayscale_url.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
-# Usage: ./gen_url.rb
 require 'dragonfly'
 Dragonfly.app.configure do
   verify_urls false
+  plugin :imagemagick
 end
 uid = Dragonfly.app.store(File.open('lena_std.png'))
-url_path = Dragonfly.app.fetch(uid).url
+url_path = Dragonfly.app.fetch(uid).convert('-type Grayscale').url
 puts "http://localhost:9292" + url_path
