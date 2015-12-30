24f5740127962ac596b7a66bde774cd5a857427a
Kensuke Nagae <kyanny@gmail.com>
2015-12-31 00:13:19 +0900

    Update

--- clj-data.csv/src/clj_data/csv.clj
+++ clj-data.csv/src/clj_data/csv.clj
@@ -2,9 +2,26 @@
 
 (require '[clojure.data.csv :as csv])
 
-(doseq [row (csv/read-csv (slurp "usa-states.csv"))]
-  (prn (str (first row) " => " (second row))))
+(let [filename "usa-states.csv"
+      csv (slurp filename)
+      rows (csv/read-csv csv)]
+  (doseq [row rows]
+    (let [code (first row)
+          name (second row)]
+      (println (str code " => " name)))))
 
-(map
- (fn [row] (str (first row) " => " (second row)))
- (csv/read-csv (slurp "usa-states.csv")))
+(let [filename "test.ltsv"
+      csv (slurp filename)
+      rows (csv/read-csv csv :separator \tab)]
+  (map (fn [row]
+         (str (first row) " => " (second row)))
+       rows))
+
+(with-out-str
+  (csv/write-csv *out* [["foo" "bar"]
+                        ["baz" "qux"]]))
+
+(csv/write-csv *out* [["foo" "bar"]
+                      ["baz" "qux"]
+                      ["I'm" "pen"]]
+               :separator \tab)
