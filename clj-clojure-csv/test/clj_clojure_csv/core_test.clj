8a6b145818c2ae9516f21f1f27e98c274299f063
Kensuke Nagae <kyanny@gmail.com>
2016-01-03 02:27:15 +0900

    init

---
+++ clj-clojure-csv/test/clj_clojure_csv/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-clojure-csv.core-test
+  (:require [clojure.test :refer :all]
+            [clj-clojure-csv.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
