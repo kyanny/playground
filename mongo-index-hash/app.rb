350133470d66d8e707bdd1250bce210ce9b2a4d3
Kensuke Nagae <kyanny@gmail.com>
2016-03-07 14:02:29 +0900

    .

---
+++ mongo-index-hash/app.rb
@@ -0,0 +1,23 @@
+require 'active_model'
+require 'mongo_mapper'
+require 'pp'
+MongoMapper.database = 'testing'
+class Foo
+  include MongoMapper::Document
+  key :h, Hash
+end
+Foo.create(h: {a: 1, b: 2})
+pp Foo.where('h.a' => 1).explain
+puts '------------------------'
+class Foo
+  include MongoMapper::Document
+  ensure_index :h
+end
+pp Foo.where('h.a' => 1).explain
+puts '------------------------'
+class Foo
+  include MongoMapper::Document
+  ensure_index 'h.a'
+end
+pp Foo.where('h.a' => 1).explain
+puts '------------------------'
