108937a0740255b356466b032115a4c771688e1b
Kensuke Nagae <kyanny@gmail.com>
2016-03-08 18:50:31 +0900

    Fix

--- github-label-viewer/app.rb
+++ github-label-viewer/app.rb
@@ -3,6 +3,7 @@ require 'octokit'
 require 'slim'
 require 'active_support/all'
 require 'pp'
+require 'dotenv'
 
 before do
   Dotenv.load
@@ -11,14 +12,14 @@ end
 get '/*' do
   label = params['splat'].first
   client = Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
-  repo = ENV['repo']
+  @repo = ENV['repo']
   if label.present?
-    @label = client.label(repo, label)
+    @label = client.label(@repo, label)
   end
   if @label.present?
-    @labels = client.issues(repo, labels: @label.name).flat_map{ |issue| issue.labels }.uniq { |label| label.name }.sort_by { |label| label.name }
+    @labels = client.issues(@repo, labels: @label.name).flat_map{ |issue| issue.labels }.uniq { |label| label.name }.sort_by { |label| label.name }
   else
-    @labels = client.labels(repo).sort_by { |label| label.name }
+    @labels = client.labels(@repo).sort_by { |label| label.name }
   end
   slim :index
 end
