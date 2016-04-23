d7a86576c1bf1cb4f2bc1681f5afd5fec1921c81
Kensuke Nagae <kyanny@gmail.com>
2016-04-24 01:20:05 +0900

    commit

---
+++ the-rust-programming-language-ja/if/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    println!("Hello, world!");
+
+    let x = 5;
+
+    let y = if x == 5 {
+        10
+    } else {
+        15
+    };
+
+    println!("y = {}", y);
+
+    // let a = if 1 == 2 { 10 };
+    // println!("a = {}", a);
+}
