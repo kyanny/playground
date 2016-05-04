413ec7001c05267b60b1c62f456690b4d6b7bae7
Kensuke Nagae <kyanny@gmail.com>
2016-05-05 02:34:08 +0900

    No need this option

--- fitbit_my_first_app/config/initializers/omniauth.rb
+++ fitbit_my_first_app/config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :fitbit_oauth2, ENV['FITBIT_CONSUMER_KEY'], ENV['FITBIT_CONSUMER_SECRET'], response_type: 'code', scope: 'activity heartrate location nutrition profile settings sleep social weight', provider_ignores_state: true
+  provider :fitbit_oauth2, ENV['FITBIT_CONSUMER_KEY'], ENV['FITBIT_CONSUMER_SECRET'], response_type: 'code', scope: 'activity heartrate location nutrition profile settings sleep social weight'
 end
