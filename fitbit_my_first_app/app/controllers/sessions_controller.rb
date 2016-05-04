c69b583ba39586e82ae19bdff625001b45232090
Kensuke Nagae <kyanny@gmail.com>
2016-05-05 02:28:55 +0900

    Add app

---
+++ fitbit_my_first_app/app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :create
+
+  def new
+  end
+
+  def create
+    auth_hash = request.env['omniauth.auth']
+    access_token = auth_hash.credentials.token
+    res = RestClient.get 'https://api.fitbit.com/1/user/-/activities/heart/date/2016-05-04/1d/1sec.json', {'Authorization' => "Bearer #{access_token}"}
+    require 'pp'
+    pp JSON.parse(res)
+    render :text => auth_hash.inspect
+  end
+
+  def failure
+  end
+end
