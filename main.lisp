b6f786bb806f3f586f7fa6c0a7123e695f794363
Kensuke Nagae <kyanny@gmail.com>
2017-08-06 04:17:43 +0900

    .

---
+++ main.lisp
@@ -0,0 +1,61 @@
+(+ 1 1)
+
+(ql:system-apropos "json")
+
+(ql:quickload "cl-json")
+
+(let
+    ((x (cl-json:decode-json-from-string "{\"foo\":1}")))
+  (print (length x)))
+
+
+(ql:system-apropos "excel")
+(ql:system-apropos "github")
+
+(ql:quickload "cl-github-v3")
+
+(with-open-file (in "example.json")
+  (loop for line = (read-line in nil)
+        while line
+        do (write-line line)))
+
+
+(let*
+    ((string (alexandria:read-file-into-string
+              "example.json"))
+     (json (cl-json:decode-json-from-string string)))
+  (first (first (json))))
+
+(cl-json:decode-json-from-string
+ (alexandria:read-file-into-string "example.json"))
+
+(setq a 1)
+
+a
+
+(setq data (alexandria:read-file-into-string "example.json"))
+
+data
+
+(setq json (cl-json:decode-json-from-string data))
+
+json
+
+(json :glossary)
+
+(assoc json :glossary)
+
+json
+(size json)
+
+(length json)
+
+(first json)
+
+(assoc (first json) :GLOSSARY)
+
+
+(check-type json)
+
+(assoc :glossary json)
+(cons (assoc :glossary json))
